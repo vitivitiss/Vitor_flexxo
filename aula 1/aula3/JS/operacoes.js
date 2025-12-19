8642dcf40f1c86aab205ed3abbe8eb879b843e00
vitivitiss <vitordasilvascheffer@gmail.com>
2025-12-18 22:31:00 -0300

    Add files via upload

---
+++ aula 1/aula3/JS/operacoes.js
@@ -0,0 +1,23 @@
+let soma = 0;
+soma = 3 +5;
+console.log("A soma é " + soma );
+
+
+let sub = 0 ;
+sub = 20-2;
+console.log("A subtração é " + sub );
+
+
+let mult = 0 ;
+mult = 20*2;
+console.log("A multiplicação é " + mult );
+
+let div = 0 ;
+div = 20/2;
+console.log("A divisão é " + div );
+
+let resto = 0 ;
+resto = 33%7;
+console.log("A resto é " + resto );
+
+
---
+++ aula 1/aula3/JS/repeticao.js
@@ -0,0 +1,33 @@
+ 
+
+    for(let i = 0; i < 10 ; i++){
+
+        console.log("loop" + i );
+
+
+    }
+
+    for(let y = 0; y<= 50; y++){
+        if (y % 2 == 0){
+           console.log (y+ " é par"); 
+        }
+
+
+    }
+    let v = 0;
+
+
+    while(v < 100){
+        v++;
+        console.log("loop:" + v);
+
+
+
+
+    }
+    do{
+
+        v++;
+        console.log("loop:" + v);
+
+    }while (v < 100)
